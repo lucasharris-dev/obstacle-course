61edf9f7ea75a9821b32fc25bc198c2ae713d6d0
Lucas Harris <lucasharris.dev@outlook.com>
2023-04-29 19:03:20 -0400

    Add player input movement

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,10 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0.01f, 0f, 0f);
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float yValue = Input.GetAxis("Jump") * Time.deltaTime;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(xValue, yValue, zValue);
     }
 }
